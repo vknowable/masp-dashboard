084cb2f6487811e0d4daef5510eba0d228f7f8c7
neocybereth <sean@arrowburn.co.nz>
2025-03-14 13:03:33 +1300

    fix: update assets table

--- namada-masp-dashboard/src/components/assetTable/AssetRow.tsx
+++ namada-masp-dashboard/src/components/assetTable/AssetRow.tsx
@@ -1,81 +1,88 @@
-import { formatNumber, denomAmount } from '../../utils/numbers'
-import { RegistryAsset } from '../../types/chainRegistry'
-import { TransformedTokenSupply } from '../../api/chain'
-import NetChangeSpans from './NetChangeSpans'
-import '../../styles/shared.css';
+import { formatNumber, denomAmount } from "../../utils/numbers";
+import { RegistryAsset } from "../../types/chainRegistry";
+import { TransformedTokenSupply } from "../../api/chain";
+import NetChangeSpans from "./NetChangeSpans";
+import "../../styles/shared.css";
 
 interface AssetRowProps {
-    token: RegistryAsset
-    tokenPrice: number | null
-    tokenSupplies: TransformedTokenSupply | null
-    isLoading?: boolean
+  token: RegistryAsset;
+  tokenPrice: number | null;
+  tokenSupplies: TransformedTokenSupply | null;
+  isLoading?: boolean;
 }
 
-function AssetRow({ token, tokenPrice, tokenSupplies, isLoading }: AssetRowProps) {
-
-    if (isLoading) {
-        return (
-            <div className="p-4">
-                <div className="animate-pulse space-y-4">
-                    {[...Array(1)].map((_, i) => (
-                        <div key={i} className="h-[96px] bg-gray-700 rounded" />
-                    ))}
-                </div>
-            </div>
-        )
-    }
-
-    // TODO: Add loading state for token supplies, so we can still show the token symbol and icon
-    if (!tokenSupplies) {
-        return (
-            <div className="p-4">
-                <div className="animate-pulse space-y-4">
-                    {[...Array(1)].map((_, i) => (
-                        <div key={i} className="h-[96px] bg-gray-700 rounded" />
-                    ))}
-                </div>
-            </div>
-        )
-    }
-
-    const rawCurrentSupply = tokenSupplies.supplies.current
-    const denomCurrentSupply = denomAmount(rawCurrentSupply, 6)
-
+function AssetRow({
+  token,
+  tokenPrice,
+  tokenSupplies,
+  isLoading,
+}: AssetRowProps) {
+  if (isLoading) {
     return (
-        <div className="h-[94px] p-4 pr-32 flex gap-12 items-center bg-[#010101] rounded-tl-[5px] rounded-bl-[5px]">
-            {/* Token Column */}
-            <div className="flex items-center space-x-3">
-                {/* Asset Icon */}
-                <div className="w-[40px] h-[40px] rounded-full overflow-hidden bg-gray-700 flex-shrink-0">
-                    {token.logo_URIs?.svg && (
-                        <img
-                            src={token.logo_URIs.svg}
-                            alt={`${token.symbol} logo`}
-                            className="w-[40px] h-[40px] object-cover"
-                            height={40}
-                            width={40}
-                        />
-                    )}
-                </div>
+      <div className="p-4">
+        <div className="animate-pulse space-y-4">
+          {[...Array(1)].map((_, i) => (
+            <div key={i} className="h-[96px] bg-gray-700 rounded" />
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-                {/* Token Symbol */}
-                {/* <div className="text-sm text-white">
+  // TODO: Add loading state for token supplies, so we can still show the token symbol and icon
+  if (!tokenSupplies) {
+    return (
+      <div className="p-4">
+        <div className="animate-pulse space-y-4">
+          {[...Array(1)].map((_, i) => (
+            <div key={i} className="h-[96px] bg-gray-700 rounded" />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  const rawCurrentSupply = tokenSupplies.supplies.current;
+  const denomCurrentSupply = denomAmount(rawCurrentSupply, 6);
+
+  return (
+    <div className="h-[94px] p-4 pr-32 flex gap-12 items-center bg-[#010101] rounded-tl-[5px] rounded-bl-[5px]">
+      {/* Token Column */}
+      <div className="flex items-center space-x-3">
+        {/* Asset Icon */}
+        <div className="w-[40px] h-[40px] rounded-full overflow-hidden bg-gray-700 flex-shrink-0">
+          {token.logo_URIs?.svg && (
+            <img
+              src={token.logo_URIs.svg}
+              alt={`${token.symbol} logo`}
+              className="w-[40px] h-[40px] object-cover"
+              height={40}
+              width={40}
+            />
+          )}
+        </div>
+
+        {/* Token Symbol */}
+        {/* <div className="text-sm text-white">
                     {token.symbol}
                 </div> */}
-            </div>
+      </div>
 
-            {/* Total Value Column */}
-            <div className="flex-1">
-                <div className="asset-amt-text">
-                    {formatNumber(denomCurrentSupply, 6)} {token.symbol}
-                </div>
-                <div className="asset-amt-usd-text">
-                    ${(tokenPrice && denomCurrentSupply) ? formatNumber(denomCurrentSupply * tokenPrice, 2) : "--"}
-                </div>
-                <NetChangeSpans changes={tokenSupplies.supplies.changes} />
-            </div>
+      {/* Total Value Column */}
+      <div className="flex-1">
+        <div className="asset-amt-text">
+          {formatNumber(denomCurrentSupply, 2)} {token.symbol}
         </div>
-    )
+        <div className="asset-amt-usd-text">
+          $
+          {tokenPrice && denomCurrentSupply
+            ? formatNumber(denomCurrentSupply * tokenPrice, 2)
+            : "--"}
+        </div>
+        <NetChangeSpans changes={tokenSupplies.supplies.changes} />
+      </div>
+    </div>
+  );
 }
 
-export default AssetRow 
+export default AssetRow;
--- namada-masp-dashboard/src/components/assetTable/MetricsRow.tsx
+++ namada-masp-dashboard/src/components/assetTable/MetricsRow.tsx
@@ -1,90 +1,95 @@
-import { TransformedTokenAmount, TransformedTokenSupply } from "../../api/chain"
-import { RegistryAsset } from "../../types/chainRegistry"
-import { denomAmount, formatNumber } from "../../utils/numbers"
-import { ViewMode } from './AssetTableContainer'
-import NetChangeSpans from './NetChangeSpans'
+import {
+  TransformedTokenAmount,
+  TransformedTokenSupply,
+} from "../../api/chain";
+import { RegistryAsset } from "../../types/chainRegistry";
+import { denomAmount, formatNumber } from "../../utils/numbers";
+import { ViewMode } from "./AssetTableContainer";
+import NetChangeSpans from "./NetChangeSpans";
 
 interface MetricsRowProps {
-    // viewMode: ViewMode // was used for toggling between 'tabs'
-    token: RegistryAsset
-    tokenPrice: number | null
-    maspBalances: TransformedTokenAmount | null
-    transparentBalances: TransformedTokenAmount | null
-    isLoading?: boolean
+  // viewMode: ViewMode // was used for toggling between 'tabs'
+  token: RegistryAsset;
+  tokenPrice: number | null;
+  maspBalances: TransformedTokenAmount | null;
+  transparentBalances: TransformedTokenAmount | null;
+  isLoading?: boolean;
 }
 
-function MetricsRow({ token, tokenPrice, maspBalances, transparentBalances, isLoading }: MetricsRowProps) {
-
-    if (isLoading) {
-        return (
-            <div className="p-4">
-                <div className="animate-pulse space-y-4">
-                    {[...Array(1)].map((_, i) => (
-                        <div key={i} className="h-[96px] bg-gray-700 rounded" />
-                    ))}
-                </div>
-            </div>
-        )
-    }
-
-    // TODO: Add loading state for token supplies, so we can still show the token symbol and icon
-    if (!maspBalances || !transparentBalances) {
-        return (
-            <div className="p-4">
-                <div className="animate-pulse space-y-4">
-                    {[...Array(1)].map((_, i) => (
-                        <div key={i} className="h-[96px] bg-gray-700 rounded" />
-                    ))}
-                </div>
-            </div>
-        )
-    }
-
-    const rawCurrentMasp = maspBalances.balances.current
-    const denomCurrentMasp = denomAmount(rawCurrentMasp, 6)
-
-    const rawCurrentTransparent = transparentBalances.balances.current
-    const denomCurrentTransparent = denomAmount(rawCurrentTransparent, 6)
-
+function MetricsRow({
+  token,
+  tokenPrice,
+  maspBalances,
+  transparentBalances,
+  isLoading,
+}: MetricsRowProps) {
+  if (isLoading) {
     return (
-        <div className="h-[94px] p-4 flex items-center bg-[#010101] rounded-tr-[5px] rounded-br-[5px]">
-
-            {/* Shielded Value Column */}
-            <div className="flex-1 pl-8">
-                <div className="asset-amt-text">
-                    {formatNumber(denomCurrentMasp, 6)} {token.symbol}
-                </div>
-                <div className="asset-amt-usd-text">
-                    ${
-                        (tokenPrice && denomCurrentMasp) ? formatNumber(denomCurrentMasp * tokenPrice, 2) : "--"
-                    }
-                </div>
-                <NetChangeSpans 
-                    changes={maspBalances.balances.changes} 
-                />
-            </div>
-
-            {/* Transparent Value Column */}
-            <div className="flex-1">
-                <div className="asset-amt-text">
-                    {formatNumber(denomCurrentTransparent, 6)} {token.symbol}
-                </div>
-                <div className="asset-amt-usd-text">
-                    ${
-                        (tokenPrice && denomCurrentTransparent) ? formatNumber(denomCurrentTransparent * tokenPrice, 2) : "--"
-                    }
-                </div>
-                <NetChangeSpans 
-                    changes={transparentBalances.balances.changes} 
-                />
-            </div>
-
-            {/* Rewards Param Column */}
-            <div className="w-[150px] text-[#FFFF00] flex items-center justify-center">
-                xxxxxxxx
-            </div>
+      <div className="p-4">
+        <div className="animate-pulse space-y-4">
+          {[...Array(1)].map((_, i) => (
+            <div key={i} className="h-[96px] bg-gray-700 rounded" />
+          ))}
         </div>
-    )
+      </div>
+    );
+  }
+
+  // TODO: Add loading state for token supplies, so we can still show the token symbol and icon
+  if (!maspBalances || !transparentBalances) {
+    return (
+      <div className="p-4">
+        <div className="animate-pulse space-y-4">
+          {[...Array(1)].map((_, i) => (
+            <div key={i} className="h-[96px] bg-gray-700 rounded" />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  const rawCurrentMasp = maspBalances.balances.current;
+  const denomCurrentMasp = denomAmount(rawCurrentMasp, 6);
+
+  const rawCurrentTransparent = transparentBalances.balances.current;
+  const denomCurrentTransparent = denomAmount(rawCurrentTransparent, 6);
+
+  return (
+    <div className="h-[94px] p-4 flex items-center bg-[#010101] rounded-tr-[5px] rounded-br-[5px]">
+      {/* Shielded Value Column */}
+      <div className="flex-1 pl-8">
+        <div className="asset-amt-text">
+          {formatNumber(denomCurrentMasp, 2)} {token.symbol}
+        </div>
+        <div className="asset-amt-usd-text">
+          $
+          {tokenPrice && denomCurrentMasp
+            ? formatNumber(denomCurrentMasp * tokenPrice, 2)
+            : "--"}
+        </div>
+        <NetChangeSpans changes={maspBalances.balances.changes} />
+      </div>
+
+      {/* Transparent Value Column */}
+      <div className="flex-1">
+        <div className="asset-amt-text">
+          {formatNumber(denomCurrentTransparent, 2)} {token.symbol}
+        </div>
+        <div className="asset-amt-usd-text">
+          $
+          {tokenPrice && denomCurrentTransparent
+            ? formatNumber(denomCurrentTransparent * tokenPrice, 2)
+            : "--"}
+        </div>
+        <NetChangeSpans changes={transparentBalances.balances.changes} />
+      </div>
+
+      {/* Rewards Param Column */}
+      <div className="w-[150px] text-[#FFFF00] flex items-center justify-center">
+        xxxxxxxx
+      </div>
+    </div>
+  );
 }
 
-export default MetricsRow 
+export default MetricsRow;
