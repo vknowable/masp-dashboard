207792e29dd6e8923f0172b63a04ff898b6f458f
iskay <ikay@lakeheadu.ca>
2025-05-15 18:37:55 +0000

    add react-router and estimator page

---
+++ namada-masp-dashboard/src/pages/Dashboard.tsx
@@ -0,0 +1,35 @@
+import Header from "../components/Header";
+import InfoGridContainer from "../components/infoGrid/InfoGridContainer";
+import AssetTableContainer from "../components/assetTable/AssetTableContainer";
+import IbcChannelsContainer from "../components/ibcChannels/IbcChannelsContainer";
+import ChartContainer from "../components/chart/ChartContainer";
+
+interface DashboardProps {
+    darkMode: boolean;
+    setDarkMode: (value: boolean) => void;
+}
+
+const Dashboard = ({ darkMode, setDarkMode }: DashboardProps) => {
+    return (
+        <div className="min-h-screen flex flex-col p-[24px] bg-white dark:bg-[#121212] text-black dark:text-white">
+            <header className="flex justify-between items-center">
+                <Header />
+                {/* <button
+                    onClick={() => setDarkMode(!darkMode)}
+                    className="p-2 rounded-md bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors"
+                >
+                    {darkMode ? '☀️' : '🌙'}
+                </button> */}
+            </header>
+
+            <main className="flex-1 pb-16">
+                <InfoGridContainer />
+                <ChartContainer />
+                <AssetTableContainer />
+                <IbcChannelsContainer />
+            </main>
+        </div>
+    );
+};
+
+export default Dashboard;
--- namada-masp-dashboard/vite.config.ts
+++ namada-masp-dashboard/vite.config.ts
@@ -5,17 +5,22 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+    plugins: [react(), wasm(), topLevelAwait()],
 
-  server: {
-    fs: {
-      allow: ["../pkg", "./"],
+    server: {
+        fs: {
+            allow: ["../pkg", "./"],
+        },
     },
-  },
 
-  // Below is set according to https://github.com/Menci/vite-plugin-wasm#usage
-  // to support top-level `await`s
-  build: {
-    target: "esnext",
-  },
+    // Below is set according to https://github.com/Menci/vite-plugin-wasm#usage
+    // to support top-level `await`s
+    build: {
+        target: "esnext",
+    },
+
+    // Handle client-side routing during development and production
+    preview: {
+        port: 5173,
+    }
 });
